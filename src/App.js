6869444b01a2dba526fb645580ee84fac8ed82b0
Hunter Leachman <Leachmanh@gmail.com>
2018-05-09 06:25:23 -0500

    initial commit, pages 1-53 from The Road To Learn React

---
+++ src/App.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const list = [
+  {
+    title: 'React',
+    url: 'https://facebook.github.io/react/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://github.com/reactjs/redux',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      list: list,
+      searchTerm: '',
+    };
+    this.onSearchChange = this.onSearchChange.bind(this);
+    this.onDismiss = this.onDismiss.bind(this);
+  }
+  onDismiss(id){
+      const isNotId = item => item.objectID !== id;
+      const updatedList = this.state.list.filter(isNotId);
+      this.setState({ list: updatedList});
+  }
+  onSearchChange(event){
+    this.setState({ searchTerm: event.taget.value });
+  }
+  render() {
+    return (
+      <div className="App">
+        <form>
+          <input
+            type="text"
+            onChange={this.onSearchChange}
+          />
+        </form>
+        {this.state.list.map(item =>
+            <div key={item.objectID} >
+              <span>
+                <a href={item.url}> {item.title}</a>
+              </span>
+              <span>{item.autor}</span>
+              <span>{item.num_comments}</span>
+              <span>{item.points}</span>
+              <span>
+                <button
+                  onClick={() => this.onDismiss(item.objectID)}
+                  type="button"
+                >
+                Dismiss
+                </button>
+              </span>
+            </div>
+        )}
+      </div>
+    );
+  }
+}
+
+export default App;
